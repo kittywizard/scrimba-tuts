830dde0a1e6c62f13eb1ea7c90b6972b4ed81663
marleigh <volcana@gmail.com>
2020-07-20 16:30:31 -0700

    event listeners

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("test");
+
+let num = document.getElementById("num");
+let button = document.getElementById("button");
+
+document.addEventListener("click", eventTime);
+
+function eventTime(){
+    if(num.value === ""){
+        console.log("Nothing was entered.");
+    } else{
+        console.log(num.value);
+    }
+}
