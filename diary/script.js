f331706068b225b95c605186ff3ff4aa6e95d8a4
marleigh <volcana@gmail.com>
2020-08-04 14:45:16 -0700

    adding entries and buttons

--- diary/script.js
+++ diary/script.js
@@ -1,15 +1,25 @@
 const entryForm = document.getElementById('entryForm');
 const entrySection = document.getElementById('entries');
 const text = document.getElementsByClassName('entry-textbox'); //this returns a collection which is basically an array
+const entriesNav = document.querySelector('.entries-nav');
 
 function addEntry(e){
 
     let entryDiv = document.createElement('div');
     entryDiv.className = "single-entry";
     entryDiv.innerText = text[0].value;
-    console.log(entryDiv);
-
+    entryDiv.style.display = "none";
+    entrySection.style.display = "block";
     entrySection.appendChild(entryDiv);
+
+    //clear text box
+    text[0].value = '';
+
+    const displayButton = document.createElement("button");
+    displayButton.className = "entry-button";
+    displayButton.innerText = 1;
+    entriesNav.appendChild(displayButton);
+
     e.preventDefault();
 }
 
