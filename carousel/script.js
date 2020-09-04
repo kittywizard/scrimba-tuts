c9afdf4663f6cc831e3c77c34858d20500ab2cf3
marleigh <volcana@gmail.com>
2020-09-03 19:13:16 -0700

    fully functional kitten carousel!

--- carousel/script.js
+++ carousel/script.js
@@ -10,10 +10,37 @@ let slidePosition = 0;
 const totalSlides = slides.length;
 
 function moveToNextSlide() {
-    console.log("next");
-    //when clicked
+    
+    hideSlides();
+
+    if(slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    //add visible class to next slide
+    slides[slidePosition].classList.add('visible');
 }
 
 function moveToPrevSlide() {
-    console.log("prev");
+    hideSlides();
+
+    if(slidePosition === 0) {
+       
+        slidePosition = totalSlides - 1;
+        console.log(slidePosition);
+
+    } else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add('visible');
+}
+
+function hideSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('visible');
+        slide.classList.add('hidden');
+    }
 }
