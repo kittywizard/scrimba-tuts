7270f3e17276013e00cf37f264d2cd115919fb3d
marleigh <volcana@gmail.com>
2020-10-27 10:45:46 -0700

    palindrome challenge

---
+++ js-challenges/palindrome/script.js
@@ -0,0 +1,17 @@
+function caseInsensitivePalindrome(str) {
+    //  write code here.
+    const str2 = str.toLowerCase();
+    const strArr = str2.split('').reverse().join('');
+    console.log(str2);
+    console.log(strArr);
+
+    //i think the key here is to not actually make the original string an array
+    //but simply reverse it and return it as a string to compare
+
+    return str2 === strArr;
+
+}
+
+let str = "tacocats";
+
+caseInsensitivePalindrome(str);
