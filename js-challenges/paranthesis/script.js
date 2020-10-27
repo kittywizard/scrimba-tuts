4f8eb33df701917b1966f2e7ddf6314ee7ec7913
marleigh <volcana@gmail.com>
2020-10-27 14:35:35 -0700

    factorial challenge

--- js-challenges/paranthesis/script.js
+++ js-challenges/paranthesis/script.js
@@ -1,13 +1,32 @@
 // 3 different ways
 
 //1
+//regular method
 //function encloseInBrackets(str)  {return str = "(" + str + ")"}
 
 //2
+//ES6 method
 //function encloseInBrackets(str) {return str = `(${str})`}
 
 //3
+//using concat() 
 //function encloseInBrackets(str) {return str = '('.concat(str, ')');}
 
-let str = "hello";
-console.log(encloseInBrackets(str));
+// let str = "hello";
+// console.log(encloseInBrackets(str));
+
+function factorialNumber(num) {
+    //factorial is num * each number less than itself
+    let factorial = 1; 
+
+    for (let i = 1; i < num; i++) {
+        //for every iteration, we need to multiply i by .. ??
+        factorial *= (i+1);
+        console.log(factorial);
+    }
+
+    return factorial;
+}
+
+let num = 5;
+console.log(factorialNumber(num));
