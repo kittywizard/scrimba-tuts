1559ce5f80099488bf71d0365b09bf8dd6bee753
marleigh <volcana@gmail.com>
2020-10-29 17:02:32 -0700

    logo editor beginnings

--- js-challenges/palindrome/script.js
+++ js-challenges/palindrome/script.js
@@ -1,17 +1,26 @@
-function caseInsensitivePalindrome(str) {
+// function caseInsensitivePalindrome(str) {
+//     //  write code here.
+//     const str2 = str.toLowerCase();
+//     const strArr = str2.split('').reverse().join('');
+//     console.log(str2);
+//     console.log(strArr);
+
+//     //i think the key here is to not actually make the original string an array
+//     //but simply reverse it and return it as a string to compare
+
+//     return str2 === strArr;
+
+// }
+
+// let str = "tacocats";
+
+// caseInsensitivePalindrome(str);
+
+function firstDigit(str) {
     //  write code here.
-    const str2 = str.toLowerCase();
-    const strArr = str2.split('').reverse().join('');
-    console.log(str2);
-    console.log(strArr);
-
-    //i think the key here is to not actually make the original string an array
-    //but simply reverse it and return it as a string to compare
-
-    return str2 === strArr;
-
+    const regex = /\d/g;
+        
+    const results = str.match(regex);
+    return results[0];
+    
 }
-
-let str = "tacocats";
-
-caseInsensitivePalindrome(str);
