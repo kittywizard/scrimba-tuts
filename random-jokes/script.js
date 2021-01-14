9e7afd9e41ac0d4904e599bb473f7af3d72fd3c0
marleigh <volcana@gmail.com>
2021-01-14 12:16:15 -0800

    works - needs some refinement on the creative end

--- random-jokes/script.js
+++ random-jokes/script.js
@@ -8,10 +8,21 @@ const newJokeBtn = document.getElementById('getNewJoke');
 newJokeBtn.addEventListener('click', getJoke);
 
 async function getJoke() {
+    punchlineDiv.innerText = '';
+
     const promise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
     const jokes = await promise.json();
 
-    console.log(jokes);
+    let setup = jokes[0].setup;
+    let punchline = jokes[0].punchline;
 
-    
+    setupDiv.innerText = setup;
+
+    punchlineBtn.addEventListener('click', () => {
+        punchlineDiv.innerText = punchline;
+        punchlineBtn.classList.toggle('hidden'); //fix the button logic
+        });
 }
+
+getJoke();
+
