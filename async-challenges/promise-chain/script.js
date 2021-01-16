144a82e83e87f48e2f0b79fd1549dd2a50f36de3
marleigh <volcana@gmail.com>
2021-01-16 13:02:26 -0800

    async challenge start

---
+++ async-challenges/promise-chain/script.js
@@ -0,0 +1,13 @@
+document.getElementById("action").addEventListener("click", ()=>{
+    
+    // call user API
+    // store random first name in a variable firstUser
+    // call user API again, after you finish calling first one
+    // store random first name in variable secondUser 
+    // console log `${firstUser} and ${secondUser} are friends`
+    
+    const userApi = 'https://randomuser.me/api/'
+    
+    })
+    
+    async
