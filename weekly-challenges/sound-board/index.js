818da5c92d00f4e6fa1f788f9e3f27d1b8818240
marleigh <volcana@gmail.com>
2021-05-14 11:01:02 -0700

    creating a sound board game

--- weekly-challenges/sound-board/index.js
+++ weekly-challenges/sound-board/index.js
@@ -1,18 +1,33 @@
+const scoreDisplay = document.querySelector('.score');
+let score = 0;
+
 const btn = document.querySelectorAll('.btn');
 btn.forEach(btn => {
     btn.addEventListener('click', () => {
-        playSound(btn.id);
-        });
+        listen(btn.id)
+    });
 });
 
-function playSound(id) {
-    //stop any audio that's playing before starting a new one??
-    
-    let audio = new Audio(`/sounds/${id}.mp3`);
-    audio.play();
+const playBtn = document.getElementById('play');
+playBtn.addEventListener('click', playSound);
+
+//need to reset this every time duh
+let randoNum = Math.floor((Math.random() * ((btn.length - 1) - 1 + 1)) + 1);
+
+function playSound() {
+    let audio = new Audio(`sounds/${btn[randoNum].id}.mp3`);
+
+    //wait a short second before playing
+    setTimeout(() => audio.play(), 1000);
+
 }
 
-//other goals:
-    //add new sounds
-    //turn into a game? 
-    
+
+function listen(id) {
+    console.log(`ID of clicked button: ${id} and ID of random audio: ${btn[randoNum].id}.`);
+    if(id === btn[randoNum].id) {
+        //ta da you win!
+        score++;
+        scoreDisplay.textContent = score;
+    }
+}
