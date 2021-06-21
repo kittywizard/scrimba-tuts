927653530467becd739a3e062fedda3e563233be
marleigh <volcana@gmail.com>
2021-06-21 16:12:50 -0700

    radio buttons coming together

--- weekly-challenges/tip-calc/script.js
+++ weekly-challenges/tip-calc/script.js
@@ -1,36 +1,30 @@
 
 const form = document.getElementById('tipCalc');
 const results = document.querySelector('.results');
-const tip15 = document.getElementById('15percent');
-const tip18 = document.getElementById('18percent');
-const tip20 = document.getElementById('20percent');
+const customDiv = document.querySelector(".custom");
 
-// tip15.addEventListener('click', () => {
-//     tip15.classList.add('selected');
-//     //need to deactivate other buttons
-// });
-
-// tip18.addEventListener('click', () => {
-//     tip18.classList.add('selected');
-// });
-
-// tip20.addEventListener('click', () => {
-//     tip20.classList.add('selected');
-// });
+const customTip = document.getElementById('custom-tip');
+customTip.addEventListener('click', () => {
+    console.log('test');
+    customDiv.classList.toggle('hide');
+});
 
 form.addEventListener('submit', e => {
     let newData = new FormData(e.target);
     let check = newData.get('checkAmount');
-
-    //check for if a button is selected
-
     let tip = newData.get('tipAmount');
-    console.log(tip)
-    //let tip = newData.get('customTip');
-    
-    
+    console.log('tipAmount is currently set to ' + tip)
+    console.log(typeof tip)
 
-    getData(check, tip);
+    if(Number(tip) === 0) {
+        tip = newData.get('customTip');
+        console.log(newData.get(customTip));
+        //getData(check, tip);
+    } else {
+        console.log(tip);
+        //getData(check, tip);
+    }
+
     e.preventDefault();
 });
 
@@ -57,31 +51,3 @@ function getData(check, tip) {
                         <hr>
                         <strong>Total:</strong> $${totalBill}`;
 }
-
-function setTipAmount(tip, btn) {
-    btn.classList.add('selected');
-    //deselect other buttons - remove eventlisteners
-
-
-
-    // let checkDisplay = document.createElement('span');
-    // checkDisplay.classList.add('result-display');
-    // checkDisplay.textContent = `Check: ${check}`;
-
-    // let tipDisplay = document.createElement('span');
-    // tipDisplay.classList.add('result-display');
-    // tipDisplay.textContent = `Tip Amount: ${tipFinal}`;
-
-
-    // let totalDisplay = document.createElement('span');
-    // totalDisplay.classList.add('result-display');
-    // totalDisplay.textContent = `Total: ${totalBill}`;
-
-    // results.appendChild(checkDisplay);
-    // results.appendChild(tipDisplay);
-    // results.appendChild(totalDisplay);
-}
-
-//need to activate the function with any button click - but need to make sure that the check amount has been filled in 
-
-//
