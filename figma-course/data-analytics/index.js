09301ffc4d8755d2f4b910f0fc0f0dcaff1c1de4
marleigh <volcana@gmail.com>
2022-05-30 14:34:16 -0700

    mobile styles done, adding media queries

--- figma-course/data-analytics/index.js
+++ figma-course/data-analytics/index.js
@@ -1,1 +1,10 @@
-// javascript
+const hamburger = document.getElementById('hamburger');
+const arrow = document.getElementById('arrow');
+
+hamburger.addEventListener('click', () => {
+    console.log('display side menu;');
+})
+
+arrow.addEventListener('click', () => {
+    console.log('hide menu')
+})
