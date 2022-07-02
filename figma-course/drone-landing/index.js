4b7e4d79938486cff5edf3256f46cde4d68164d9
marleigh <volcana@gmail.com>
2022-07-02 13:32:28 -0700

    some basic menu functionality

--- figma-course/drone-landing/index.js
+++ figma-course/drone-landing/index.js
@@ -1,1 +1,17 @@
-// javascript
+const hamburgerBtn = document.querySelector('.hamburger');
+const navClose = document.querySelector('.menu-close');
+const nav = document.querySelector('.menu');
+
+hamburgerBtn.addEventListener('click', showNav);
+navClose.addEventListener('click', hideNav);
+
+function showNav() {    
+    nav.style.display = "block";
+    hamburgerBtn.style.display = 'none';
+}
+
+function hideNav() {
+    nav.style.display = 'none';
+    hamburgerBtn.style.display = 'block';
+
+}
